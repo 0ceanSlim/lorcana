85d6e42be9e7811c0b7c8c62991b6b183e77d548
0ceanSlim <chriskerr@gmx.com>
2023-11-14 11:30:28 -0500

    name folder set names specefied by

--- saveImages.py
+++ saveImages.py
@@ -2,10 +2,15 @@ import os
 import pandas as pd
 import requests
 import re
+import json
 
 # Function to clean filename by removing problematic characters
 def clean_filename(filename):
     return re.sub(r'[\\/*?:"<>|]', '', filename)
+
+# Load set names from JSON file
+with open('sets.json', 'r') as file:
+    set_names_mapping = json.load(file)
 
 # Read the CSV file
 data = pd.read_csv('data.csv')
@@ -17,19 +22,22 @@ os.makedirs(images_folder, exist_ok=True)
 # Iterate through the rows
 for index, row in data.iterrows():
     card_set_id = row['card_set_id']
-    card_id = row['id']
+    set_id = row['number']
     name = row['name']
     image_url = row['image']
 
-    # Create a folder for the card_set_id if it doesn't exist
-    card_set_folder = os.path.join(images_folder, str(card_set_id))
+    # Get set name from mapping
+    set_name = set_names_mapping.get(str(card_set_id), f"set_{card_set_id}")
+
+    # Create a folder for the set_name if it doesn't exist
+    card_set_folder = os.path.join(images_folder, set_name)
     os.makedirs(card_set_folder, exist_ok=True)
 
     # Get the image
     response = requests.get(image_url)
     if response.status_code == 200:
         # Clean the filename
-        image_filename = clean_filename(f"{card_id} - {name}.webp")  # Adjust the extension if needed
+        image_filename = clean_filename(f"{set_id} - {name}.webp")  # Adjust the extension if needed
         image_path = os.path.join(card_set_folder, image_filename)
         with open(image_path, 'wb') as f:
             f.write(response.content)
